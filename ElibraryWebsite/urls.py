812b6353162e2a0e79c46e4a98333b8c8c8eacd1
= <=>
2024-02-26 06:35:38 +0300

    t1

--- ElibraryWebsite/urls.py
+++ ElibraryWebsite/urls.py
@@ -19,6 +19,11 @@ from django.urls import path
 import include
 from django.conf.urls.static import static
 
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("apps.bookmodule.urls")),
