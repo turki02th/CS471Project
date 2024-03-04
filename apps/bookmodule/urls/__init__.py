5a81e1f524af907a729d3b3a66e7e4b31c375e48
= <=>
2024-03-04 23:57:11 +0300

    t-v1.2

--- apps/bookmodule/urls/__init__.py
+++ apps/bookmodule/urls/__init__.py
@@ -2,8 +2,13 @@
 from django.urls import path ;import include
 from apps.bookmodule import views
 
-urlpatterns = [
+from django.urls import path
+from django.urls import re_path
+from apps.bookmodule import views
 
-    path('',views.index,name='index')
+
+urlpatterns = [    
+    path('', views.index, name='index'),
+    path('books', views.books),
+    path('book/<int:bId>', views.book)
 ]
-
--- apps/bookmodule/views/__init__.py
+++ apps/bookmodule/views/__init__.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
     #study the request
     # render the appropriate template for this request
     return render(request, 'bookmodule/index.html') #rendering a temlates
+
+def books(request):
+    return render(request, 'bookmodule/bookList.html')
+
+def book(request, bId):
+    
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
+    book2 = {'id':56788765, 'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    
+    targetBook = None
+    if book1['id'] == bId: targetBook = book1
+    if book2['id'] == bId: targetBook = book2
+    
+    if targetBook == None: return redirect('/books')
+    
+    context = {'book':targetBook} # book is the variable name accessible by template
+    return render(request, 'bookmodule/book.html', context)
+
