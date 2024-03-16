a2ba16da674626417b0965ffbf5be08389a769f0
= <=>
2024-03-16 15:01:13 +0300

    t-v1.5

--- ElibraryWebsite/urls.py
+++ ElibraryWebsite/urls.py
@@ -22,6 +22,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -29,4 +32,8 @@ urlpatterns = [
     path("", include("apps.bookmodule.urls")),
     
 
+
 ]
+
+urlpatterns == static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
